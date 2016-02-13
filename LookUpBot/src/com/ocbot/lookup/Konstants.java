dceb81a78580654815ab2f189955cbd6aa26f1f4
maddy <maddy@teamchat.com>
2016-02-13 12:34:10 +0800

    modified api key usage

--- LookUpBot/src/com/ocbot/lookup/Konstants.java
+++ LookUpBot/src/com/ocbot/lookup/Konstants.java
@@ -3,5 +3,5 @@ package com.ocbot.lookup;
 public class Konstants
 {
 
-	public static final String MASHAPE_API_KEY = "C8XE1DZWPmmshf3pUtsEKA4jEdZYp1nsJlijsn4yGa0HEvxLuK";
+	public static final String MASHAPE_API_KEY = "<add your mashape API key>";
 }
